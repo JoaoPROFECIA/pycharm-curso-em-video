e149e342b9dcd7187ed5b8b555868ebef4c1c7b9
Joao Victor <victorjv3288@gmail.com>
2022-05-26 11:13:04 -0300

    Create aula21.py

---
+++ pythonProject/Curso Mundo 3/aula21.py
@@ -0,0 +1,38 @@
+#def fatorial(n=1):
+#    if n == 0:
+#        return 1
+#    else:
+#        return n * fatorial(n - 1)
+
+
+#f1 = fatorial(5)
+#f2 = fatorial(4)
+#f3 = fatorial()
+#print(f'Os valores são {f1}, {f2} e {f3}')
+
+
+#def parOuImpar(n=0):
+#    if n % 2 == 0:
+#        return 'par'
+#    else:
+#        return 'impar'
+
+
+#num = int(input('Digite um número: '))
+#print(f'O número {num} é {parOuImpar(num)}')
+
+
+def votoObrigatorio(ano=0):
+    from datetime import date
+    anoAtual = date.today().year
+    idade = anoAtual - ano
+    if idade < 16:
+        print(f'Com {idade} anos, não vota.')
+    elif idade >= 16 and idade < 18 or idade > 65:
+        print(f'Com {idade} anos, vota opcional.')
+    else:
+        print(f'Com {idade} anos, vota obrigatório.')
+
+
+votoObrigatorio(int(input('Digite o ano de nascimento: ')))
+
