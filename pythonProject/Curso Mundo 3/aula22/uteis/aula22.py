353b8cec3acf069c70160b06bb719640f0db75db
Joao Victor <victorjv3288@gmail.com>
2022-06-30 10:23:04 -0300

    aula22 - módulos e pacotes

---
+++ pythonProject/Curso Mundo 3/aula22/uteis/aula22.py
@@ -0,0 +1,6 @@
+from aula22 import numeros
+
+num = int(input('Digite um número: '))
+print(f'O dobro de {num} é {numeros.dobro(num)}')
+print(f'O triplo de {num} é {numeros.triplo(num)}')
+print(f'O fatorial de {num} é {numeros.fatorial(num)}')
---
+++ pythonProject/Curso Mundo 3/aula22/uteis/numeros/__init__.py
@@ -0,0 +1,14 @@
+def fatorial(n):
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    return f
+
+
+def dobro(x):
+    return 2 * x
+
+
+def triplo(x):
+    return 3 * x
+
