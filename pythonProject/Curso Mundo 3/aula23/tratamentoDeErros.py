85bc2dea4644e73c11b990d80b84e0324da3b1df
Joao Victor <victorjv3288@gmail.com>
2022-07-04 13:46:56 -0300

    Update tratamentoDeErros.py

--- pythonProject/Curso Mundo 3/aula23/tratamentoDeErros.py
+++ pythonProject/Curso Mundo 3/aula23/tratamentoDeErros.py
@@ -2,8 +2,14 @@ try:
     a = int(input('Digite um número: '))
     b = int(input('Digite outro número: '))
     c = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou.')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero.')
+except KeyboardInterrupt:
+    print('O usuário preferiu não digitar esse número.')
 except Exception as erro:
-    print(f'Infelizmente ocorreu um erro!\nO erro foi: {erro.__class__}')
+    print(f'O erro foi {erro.__class__}')
 else:
     print(f'O resultado é {c:.1f}')
 finally:
