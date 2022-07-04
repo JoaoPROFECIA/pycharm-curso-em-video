aaebd323fbf053857ae1d0d8f193a75a633ac9cf
Joao Victor <victorjv3288@gmail.com>
2022-07-04 11:50:59 -0300

    formatação

--- pythonProject/Curso Mundo 3/ex112/utilidadescev/moeda/moeda.py
+++ pythonProject/Curso Mundo 3/ex112/utilidadescev/moeda/moeda.py
@@ -62,12 +62,12 @@ def resumo(preço=0, taxa=10, taxa2=5):
     :param taxa2:
     :return:
     '''
-    print('-' * 30)
+    print('-' * 34)
     print('RESUMO DO VALOR'.center(30))
-    print('-' * 30)
-    print(f'Preço analisado: \t{moeda(preço)}')
+    print('-' * 34)
+    print(f'Preço analisado:\t{moeda(preço)}')
     print(f'Dobro do preço: \t{dobro(preço, True)}')
-    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'Metade do preço:\t{metade(preço, True)}')
     print(f'{taxa}% de aumento: \t{aumentar(preço, taxa, True)}')
     print(f'{taxa2}% de redução: \t{diminuir(preço, taxa2, True)}')
-    print('-' * 30)
+    print('-' * 34)
